289ae75a14a754cb187410f066df225c2cc1c4df
celesjanette <celesaug1@gmail.com>
2024-03-26 19:36:18 -0400

    completed layouts

--- RestaurantRater/RateDishViewController.swift
+++ RestaurantRater/RateDishViewController.swift
@@ -5,4 +5,28 @@
 //  Created by Celes Augustus on 3/26/24.
 //
 
-import Foundation
+import UIKit
+
+class RateDishViewController: UIViewController {
+
+    //@IBOutlet weak var ratingSlider: UISlider!
+    
+    @IBOutlet weak var ratingSlider: UISlider!
+ //   @IBOutlet weak var ratingLabel: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Set initial slider value
+        ratingSlider.value = 0.0
+        
+        // Call the action method when slider value changes
+        ratingSlider.addTarget(self, action: #selector(sliderValueChanged(_:)), for: .valueChanged)
+    }
+    
+    @IBAction func sliderValueChanged(_ sender: UISlider) {
+        // Update label text to show current rating value
+        let ratingValue = Int(sender.value)
+       // ratingLabel.text = "Rating: \(ratingValue)"
+    }
+}
