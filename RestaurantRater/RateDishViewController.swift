8db329768cea6a5a5120c15bf97b8945e3cd99a4
celesjanette <celesaug1@gmail.com>
2024-03-26 20:20:46 -0400

    rate functionalty complete

--- RestaurantRater/RateDishViewController.swift
+++ RestaurantRater/RateDishViewController.swift
@@ -8,25 +8,33 @@
 import UIKit
 
 class RateDishViewController: UIViewController {
-
-    //@IBOutlet weak var ratingSlider: UISlider!
+    
     
     @IBOutlet weak var ratingSlider: UISlider!
- //   @IBOutlet weak var ratingLabel: UILabel!
-
+    
+    @IBOutlet weak var ratingLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Set initial slider value
-        ratingSlider.value = 0.0
+        ratingSlider.minimumValue = 0
+               ratingSlider.maximumValue = 5
+               
+               ratingSlider.value = 0
+               
         
-        // Call the action method when slider value changes
+        
         ratingSlider.addTarget(self, action: #selector(sliderValueChanged(_:)), for: .valueChanged)
     }
     
     @IBAction func sliderValueChanged(_ sender: UISlider) {
-        // Update label text to show current rating value
-        let ratingValue = Int(sender.value)
-       // ratingLabel.text = "Rating: \(ratingValue)"
+       
+        updateRatingLabel(value: sender.value)
+    }
+    
+    func updateRatingLabel(value: Float) {
+        let ratingValue = Int(value)
+        ratingLabel.text = "\(ratingValue)"
     }
 }
